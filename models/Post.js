a0cf15343295c6d23c30ce5847cd62f96cd4d38a
John Barker <jdabarker@gmail.com>
2021-06-30 14:03:46 +0800

    updated Post model

--- models/Post.js
+++ models/Post.js
@@ -5,22 +5,14 @@ class Post extends Model {}
 
 Post.init(
   {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
     title: {
       type: DataTypes.STRING,
-      allowNull: false,
       validate: {
         notEmpty: true,
       },
     },
     content: {
       type: DataTypes.STRING,
-      allowNull: false,
       validate: {
         notEmpty: true,
       },
