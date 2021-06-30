5938796c5e92423af03f4d53c2df08deb797685f
John Barker <jdabarker@gmail.com>
2021-06-30 18:29:52 +0800

    added post seeds

--- seed-database.js
+++ seed-database.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
+const { User, Post, Comment } = require("./models");
 const chalk = require("chalk");
 const sequelizeConnection = require("./config/connection");
 const seedUsers = require("./seeds/seed-users");
+const seedPosts = require("./seeds/seed-posts");
 
 async function seedAll() {
   await sequelizeConnection.sync({ force: true });
@@ -10,7 +12,16 @@ async function seedAll() {
   await seedUsers();
   console.log(chalk.green("\n----- USERS SEEDED -----\n"));
 
-  process.exit(0);
+  await seedPosts();
+  const posts = await Post.findAll();
+  const users = await User.findAll();
+  for (const post of posts) {
+    await post.setUser(users[0]);
+  }
+
+  console.log(chalk.green("\n----- POSTS SEEDED -----\n"));
+
+  await process.exit(0);
 }
 
 seedAll();
---
+++ seeds/seed-posts.js
@@ -0,0 +1,22 @@
+const { Post } = require("../models");
+
+const postData = [
+  {
+    title: "The answer to life, the universe, and everything",
+    content: "42",
+  },
+  {
+    title: "Best programming languages",
+    content: "JavaScript, C#, Python",
+  },
+  {
+    title: "Beach Boys",
+    content: `California Girls.
+    Sloop John B
+    Surfin' USA`,
+  },
+];
+
+const seedPosts = () => Post.bulkCreate(postData);
+
+module.exports = seedPosts;
