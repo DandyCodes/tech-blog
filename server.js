700d6c217ed4ec7c44ee8e1c657322d99349bf36
John Barker <jdabarker@gmail.com>
2021-06-30 20:11:41 +0800

    added bulma and main layout

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const routes = require("./controllers");
 const sequelizeConnection = require("./config/connection");
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 const sess = {
   secret: process.env.SESSION_SECRET
