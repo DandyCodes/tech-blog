47dec2d611a326adbb47aaf52b816fbdb8930f2b
John Barker <jdabarker@gmail.com>
2021-06-30 17:38:21 +0800

    implemented session store

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const path = require("path");
 const express = require("express");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const exphbs = require("express-handlebars");
 const routes = require("./controllers");
 const sequelizeConnection = require("./config/connection");
@@ -16,7 +18,7 @@ const sess = {
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize,
+    db: sequelizeConnection,
   }),
 };
 
