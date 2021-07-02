6f3a9534c0c646e44516df7b2b53ae98c0879c41
John Barker <jdabarker@gmail.com>
2021-07-02 16:02:17 +0800

    implemented comments on posts on homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,13 +4,15 @@ const withAuth = require("../utils/with-auth");
 
 router.get("/", async (req, res) => {
   const loggedIn = req.session.loggedIn;
-  const posts = (await Post.findAll()).map(post => post.dataValues);
+  const posts = await getPosts();
   res.render("home", { posts, loggedIn });
 });
 
 router.get("/dashboard", withAuth, async (req, res) => {
   const loggedIn = req.session.loggedIn;
-  res.render("dashboard", { loggedIn });
+  const user = await User.findByPk(req.session.userId);
+  const posts = (await user.getPosts()).map(post => post.dataValues);
+  res.render("dashboard", { posts, loggedIn });
 });
 
 router.get("/login", (req, res) => {
@@ -32,3 +34,52 @@ router.get("*", (req, res) => {
 });
 
 module.exports = router;
+
+async function getPosts() {
+  const postRows = await Post.findAll();
+  const posts = postRows.map(postRow => postRow.dataValues);
+  const posters = await getPosters(postRows);
+  const commentsArrays = await getCommentsArrays(postRows);
+  posts.forEach((post, i) => {
+    post.username = posters[i].username;
+    post.comments = commentsArrays[i];
+    post.niceDate = getNiceDate(post);
+  });
+  return posts;
+}
+
+async function getPosters(postRows) {
+  const posterRows = [];
+  for (const postRow of postRows) {
+    posterRows.push(await postRow.getUser());
+  }
+  const posters = posterRows.map(creatorRow => creatorRow.dataValues);
+  return posters;
+}
+
+async function getCommentsArrays(postRows) {
+  const commentsArrayRows = [];
+  for (const postRow of postRows) {
+    commentsArrayRows.push(await postRow.getComments());
+  }
+  const commentsArrays = [];
+  for (const commentsArrayRow of commentsArrayRows) {
+    const commentArray = commentsArrayRow.map(
+      commentArrayRow => commentArrayRow.dataValues
+    );
+    for (const comment of commentArray) {
+      const commenter = await User.findByPk(comment.UserId);
+      comment.username = commenter.username;
+    }
+    commentsArrays.push(commentArray);
+  }
+  return commentsArrays;
+}
+
+function getNiceDate(row) {
+  return row.updatedAt
+    .toString()
+    .split(" ")
+    .filter((_word, index) => index < 4)
+    .join(" ");
+}
