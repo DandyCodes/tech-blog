9227c85e934e04b1220c67c9dba115118f90a320
John Barker <jdabarker@gmail.com>
2021-07-03 02:05:16 +0800

    comments are hidden by default - updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# tech-blog
+# Tech Blog
+
+[![License](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Description:
+
+A CMS-style tech blog site where users can publish new posts and comment on existing posts. It follows the MVC paradigm in its architectural structure, using Handlebars.js as the templating language, Sequelize as the ORM, and the express-session npm package for authentication. It is deployed on Heroku.
+
+## Table of Contents:
+
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Questions](#questions)
+
+### Usage
+
+The deployed app can be viewed here: https://nameless-escarpment-79542.herokuapp.com/
+
+### License
+
+This repository is licensed under the [MIT](https://opensource.org/licenses/MIT) license.
+
+### Contributing
+
+No contributions at this time.
+
+### Questions
+
+If you have any questions, head to my [GitHub](https://github.com/DandyCodes) or send me an email at jdabarker@gmail.com.
--- public/js/home.js
+++ public/js/home.js
@@ -4,53 +4,43 @@ const posts = Array.from(document.querySelectorAll(".post"));
 document.onreadystatechange = init;
 
 function init(event) {
-  const loggedIn = document.querySelector("main").dataset.loggedIn;
-  if (loggedIn) {
-    resetPosts(event);
-    Array.from(
-      document.querySelectorAll(".cancel-create-comment-button")
-    ).forEach(cancelButton => (cancelButton.onclick = resetPosts));
-    Array.from(document.querySelectorAll(".create-comment-button")).forEach(
-      createCommentButton =>
-        (createCommentButton.onclick = confirmCreateComment)
-    );
-    Array.from(document.querySelectorAll(".delete-comment-button")).forEach(
-      deleteCommentButton => (deleteCommentButton.onclick = deleteComment)
-    );
-  } else {
-    posts.forEach(post => {
-      post.onclick = loggedOutPostClicked;
-      post.style.cursor = "pointer";
-    });
-  }
-}
-
-function loggedOutPostClicked() {
-  document.location.replace("/login");
+  resetPosts(event);
+  Array.from(document.querySelectorAll(".close-comments-button")).forEach(
+    closeButton => (closeButton.onclick = resetPosts)
+  );
+  Array.from(document.querySelectorAll(".create-comment-button")).forEach(
+    createCommentButton => (createCommentButton.onclick = confirmCreateComment)
+  );
+  Array.from(document.querySelectorAll(".delete-comment-button")).forEach(
+    deleteCommentButton => (deleteCommentButton.onclick = deleteComment)
+  );
 }
 
 function resetPosts(event) {
   event.stopPropagation();
   posts.forEach(post => {
-    post.onclick = readyCreateComment;
+    post.onclick = showComments;
     post.style.cursor = "pointer";
-    hide(post.querySelector(".new-comment"));
+    hide(post.querySelector(".comments"));
   });
 }
 
-function readyCreateComment(event) {
+function showComments(event) {
   resetPosts(event);
   const post = event.currentTarget.closest(".post");
   post.onclick = null;
   post.style.cursor = "initial";
-  const newComment = post.querySelector(".new-comment");
-  show(newComment);
+  show(post.querySelector(".comments"));
   const newCommentTextArea = post.querySelector(".new-comment-textarea");
   newCommentTextArea.value = "";
   newCommentTextArea.focus();
 }
 
 function confirmCreateComment() {
+  const loggedIn = document.querySelector("main").dataset.loggedIn;
+  if (!loggedIn) {
+    document.location.replace("/login");
+  }
   const post = this.closest(".post");
   const body = getCommentBody(post);
   if (JSON.parse(body).content) {
@@ -62,7 +52,7 @@ function confirmCreateComment() {
 function removeAllEventListeners() {
   posts.forEach(post => {
     post.onclick = null;
-    post.querySelector(".cancel-create-comment-button").onclick = null;
+    post.querySelector(".close-comments-button").onclick = null;
     post.querySelector(".create-comment-button").onclick = null;
     Array.from(post.querySelectorAll(".delete-comment-button")).forEach(
       deleteCommentButton => (deleteCommentButton.onclick = null)
